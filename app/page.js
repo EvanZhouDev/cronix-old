4b399d1fe56dfb21b4526a00cd8a09b37122a122
EvanZhouDev <evanzhouyifan@gmail.com>
2023-04-11 12:44:05 -0700

    🚧 Added spacebar testing code

--- app/page.js
+++ app/page.js
@@ -1,10 +1,41 @@
+'use client'
+import { useEffect } from "react"
 import styles from "./timer.module.css";
 
 import Bar from "./components/bar"
 import Status from "./components/status"
 import Scramble from "./components/scramble"
 import Ministats from "./components/ministats"
-export default () => {
+
+
+
+export default function Page() {
+  useEffect(() => {
+    let downTimer
+    function handleKeyDown(e) {
+      console.log(e)
+      if (e.key === " ") {
+        downTimer = setInterval(function () {
+          console.log("AHHH")
+        }, 1);
+      }
+    }
+    function handleKeyUp(e) {
+      if (e.key === " ") {
+        clearInterval(downTimer)
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    // Don't forget to clean up
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyDown);
+      clearInterval(downTimer)
+    }
+  }, []);
+
   return (
     <div className={styles.timerPage}>
       <div className={styles.vsection}>
