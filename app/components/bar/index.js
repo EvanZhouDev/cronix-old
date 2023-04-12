6b6601574903a50ee7cc2280b8748c492155bc3d
EvanZhouDev <evanzhouyifan@gmail.com>
2023-04-12 12:29:08 -0700

    🧑‍💻 Lifted state of settings/options up!

--- app/components/bar/index.js
+++ app/components/bar/index.js
@@ -1,111 +1,24 @@
 "use client"
 // import useLocalStorage from "use-local-storage";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./bar.module.css"
 import Selection from "./selection.js"
 import Toggle from "./toggle.js"
 import Divider from "./divider.js"
-import { FiBox, FiWatch, FiMic, FiMoreVertical } from "react-icons/fi";
-import useLocalStorage from "../src/useLocalStorage.js";
 import Dropdown from "./dropdown.js"
 
-export default function Bar() {
-    let settings = [{
-        name: "event",
-        types: [
-            {
-                name: "3x3",
-                icon: <FiBox size={15} />
-            },
-            {
-                name: "4x4",
-                icon: <FiBox size={15} />
-            },
-            {
-                name: "More",
-                icon: <FiMoreVertical size={15} />,
-                submenu: [
-                    {
-                        name: "2x2",
-                        icon: <FiBox size={15} />
-                    },
-                    {
-                        name: "3x3",
-                        icon: <FiBox size={15} />
-                    },
-                    {
-                        name: "4x4",
-                        icon: <FiBox size={15} />
-                    },
-                    {
-                        name: "5x5",
-                        icon: <FiBox size={15} />
-                    },
-                    {
-                        name: "6x6",
-                        icon: <FiBox size={15} />
-                    },
-                    {
-                        name: "7x7",
-                        icon: <FiBox size={15} />
-                    },
-                ]
-            },
-        ]
-    },
-    {
-        name: "input",
-        types: [
-            {
-                name: "Timer",
-                icon: <FiWatch size={15} />
-            },
-            {
-                name: "Stackmat",
-                icon: <FiMic size={15} />
-            }
-        ]
-    }]
-
-    // const [event, setEvent, removeEvent] = useLocalStorage('event', '3x3');
-    // // let [event, setEvent] = useLocalStorage("event", "3x3")
-    // console.log(event)
-    // let [input, setInput] = useLocalStorage("input", "Timer")
-    // const [timerOptions, setTimerOptions] = useState(JSON.stringify(
-    //     {
-    //         event: null,
-    //         input: null
-    //     }
-    // ));
-
-    // useEffect(() => {
-    //     if (window.localStorage.getItem("timerOptions") === undefined) {
-    //         window.localStorage.setItem("timerOptions", JSON.stringify(
-    //             {
-    //                 event: "3x3",
-    //                 input: "Timer"
-    //             }
-    //         ));
-    //     }
-    //     setTimerOptions(JSON.parse(window.localStorage.getItem('timerOptions')));
-    // }, []);
-
-    // useEffect(() => {
-    //     window.localStorage.setItem('timerOptions', JSON.stringify(timerOptions));
-    // }, [timerOptions]);
-    const [timerOptions, setTimerOptions] = useLocalStorage(
-        'timerOptions',
-        JSON.stringify({
-            event: "3x3",
-            input: "Timer"
-        })
-    );
-
+export default function Bar({ settings: [settings, setSettings], timerOptions: [timerOptions, setTimerOptions] }) {
     let checkNew = (key, name) => {
-        let cur = JSON.parse(timerOptions);
-        cur[key] = name;
-        setTimerOptions(JSON.stringify(cur))
+        setSettings(oldSettings => {
+            let cur = JSON.parse(timerOptions);
+            cur[key] = name;
+            setTimerOptions(JSON.stringify(cur))
+            if (cur.event !== "3x3" && cur.event !== oldSettings[0].types[1]) {
+                oldSettings[0].types[1] = oldSettings[0].types[2].submenu.find(item => item.name === cur.event);
+            }
+            return oldSettings;
+        })
     }
 
     return (
@@ -128,9 +41,6 @@ export default function Bar() {
                         {i !== settings.length - 1 ? <Divider /> : null}
                     </React.Fragment>))
             }
-            {/* <Selection>
-                <Dropdown icon={<FiBox />} name="test" data={settings[0].types[2].submenu} />
-            </Selection> */}
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -5,16 +5,81 @@ import Status from "./components/status"
 import Scramble from "./components/scramble"
 import Ministats from "./components/ministats"
 import Time from "./components/time"
-
+import { useState } from "react"
 import handleTimer from "./components/src/handleTimer.js";
-
+import { FiBox, FiWatch, FiMic, FiMoreVertical } from "react-icons/fi";
+import useLocalStorage from "./components/src/useLocalStorage.js";
 export default function Page() {
+    let [settings, setSettings] = useState([{
+        name: "event",
+        types: [
+            {
+                name: "3x3",
+                icon: <FiBox size={15} />
+            },
+            {
+                name: "4x4",
+                icon: <FiBox size={15} />
+            },
+            {
+                name: "More",
+                icon: <FiMoreVertical size={15} />,
+                submenu: [
+                    {
+                        name: "2x2",
+                        icon: <FiBox size={15} />
+                    },
+                    {
+                        name: "3x3",
+                        icon: <FiBox size={15} />
+                    },
+                    {
+                        name: "4x4",
+                        icon: <FiBox size={15} />
+                    },
+                    {
+                        name: "5x5",
+                        icon: <FiBox size={15} />
+                    },
+                    {
+                        name: "6x6",
+                        icon: <FiBox size={15} />
+                    },
+                    {
+                        name: "7x7",
+                        icon: <FiBox size={15} />
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        name: "input",
+        types: [
+            {
+                name: "Timer",
+                icon: <FiWatch size={15} />
+            },
+            {
+                name: "Stackmat",
+                icon: <FiMic size={15} />
+            }
+        ]
+    }])
+
+    const [timerOptions, setTimerOptions] = useLocalStorage(
+        'timerOptions',
+        JSON.stringify({
+            event: "3x3",
+            input: "Timer"
+        })
+    );
     let [timeStatus, time, scramble] = handleTimer();
 
     return (
         <div className={styles.timerPage}>
             <div className={styles.vsection}>
-                {timeStatus !== "timing" ? <Bar /> : null}
+                {timeStatus !== "timing" ? <Bar settings={[settings, setSettings]} timerOptions={[timerOptions, setTimerOptions]} /> : null}
             </div>
             <div className={styles.vsection}>
                 {timeStatus !== "timing" ? <Scramble scramble={scramble} /> : null}
