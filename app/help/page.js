c7b10a50501cb80dd7b245daaff360c02a378016
EvanZhouDev <evanzhouyifan@gmail.com>
2023-04-13 13:24:43 -0700

    🩹 Removed unescaped quotes

--- app/help/page.js
+++ app/help/page.js
@@ -3,10 +3,9 @@ export default function Page() {
     return (
         <div className={styles.helpPage}>
             <h1>The Design Behind CRONIX</h1>
-            "Not another cube timer," you say, "We'll wind up using CSTimer again in the end"
-            <br/>
-            <br/>
-            The idea behind CRONIX is that timing and cubing should be two seperate things. We shouldn't be distracted by analyzing our data as we are doing our solves. 
+            <br />
+            <br />
+            The idea behind CRONIX is that timing and cubing should be two seperate things. We shouldn't be distracted by analyzing our data as we are doing our solves.
         </div>
     )
 }
