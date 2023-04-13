cf06f2451c49209f70e231cf86ddc35014195b62
EvanZhouDev <evanzhouyifan@gmail.com>
2023-04-13 13:26:15 -0700

    🚀 Hmm... try depolyment again?

--- app/help/page.js
+++ app/help/page.js
@@ -2,10 +2,7 @@ import styles from "./help.module.css"
 export default function Page() {
     return (
         <div className={styles.helpPage}>
-            <h1>The Design Behind CRONIX</h1>
-            <br />
-            <br />
-            The idea behind CRONIX is that timing and cubing should be two seperate things. We shouldn't be distracted by analyzing our data as we are doing our solves.
+            Coming soon!
         </div>
     )
 }
